bea4f30d6009c2c73906bcf3bc951834c1b938b8
darizae <danielarizae@gmail.com>
2023-01-10 01:31:27 +0100

    Compelte Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    let output = "";
+
+    //Iterates backwards
+    for (let i = str.length - 1; i >= 0; i--) {
+        output += str[i];
+      }
+
+    return output;
 };
 
 // Do not edit below this line

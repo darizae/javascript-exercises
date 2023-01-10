1d70f6127bef3288ef79dd25aa1ac23b4af4bd3f
darizae <danielarizae@gmail.com>
2023-01-10 18:12:03 +0100

    Complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (!isValidArg(num1) || !isValidArg(num2)) 
+        return "ERROR";
+
+    let max = Math.max(num1, num2);
+    let min = Math.min(num1, num2);
+
+    let sum = 0;
+
+    for (var i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
+//Determines if an argument is of type "number" and, if so, if it's greater than 0
+const isValidArg = function(arg) {
+    return ((typeof arg === "number") && (Number.isInteger(arg)) && (arg > 0)); 
+}
 
 // Do not edit below this line
 module.exports = sumAll;
+//module.exports = isValidArg;
--- 05_sumAll/sumAll.spec.js
+++ 05_sumAll/sumAll.spec.js
@@ -1,22 +1,43 @@
+/*const isValidArg = require('./sumAll')
+
+describe('isValidArg', () => {
+  test('returns false for non-number parameters', () => {
+    expect(isValidArg("90")).toEqual(false);
+  });
+  test('returns false for non-number parameters', () => {
+    expect(isValidArg([90, 1])).toEqual(false);
+  });
+  test('returns false for negative numbers', () => {
+    expect(isValidArg(-1)).toEqual(false);
+  });
+  test('returns true for a positive number', () => {
+    expect(isValidArg(1)).toEqual(true);
+  });
+  test('returns false for decimal positive numbers', () => {
+    expect(isValidArg(1.1)).toEqual(false);
+  });
+});
+*/
+
 const sumAll = require('./sumAll')
 
 describe('sumAll', () => {
   test('sums numbers within the range', () => {
     expect(sumAll(1, 4)).toEqual(10);
   });
-  test.skip('works with large numbers', () => {
+  test('works with large numbers', () => {
     expect(sumAll(1, 4000)).toEqual(8002000);
   });
-  test.skip('works with larger number first', () => {
+  test('works with larger number first', () => {
     expect(sumAll(123, 1)).toEqual(7626);
   });
-  test.skip('returns ERROR with negative numbers', () => {
+  test('returns ERROR with negative numbers', () => {
     expect(sumAll(-10, 4)).toEqual('ERROR');
   });
-  test.skip('returns ERROR with non-number parameters', () => {
+  test('returns ERROR with non-number parameters', () => {
     expect(sumAll(10, "90")).toEqual('ERROR');
   });
-  test.skip('returns ERROR with non-number parameters', () => {
+  test('returns ERROR with non-number parameters', () => {
     expect(sumAll(10, [90, 1])).toEqual('ERROR');
   });
 });

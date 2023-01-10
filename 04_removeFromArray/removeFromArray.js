f75b7f771d4f413e2c3db53285e31ad6de7f2b28
darizae <danielarizae@gmail.com>
2023-01-10 12:28:14 +0100

    Complete Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(first, ...rest) {
+
+    //Indefinite number of arguments are received
+    let theArray = first;
+    let toRemove = rest;
+    
+    //Returns an array where only elements that are not in the toRemove array are present
+    return theArray.filter(element => !(toRemove.includes(element)));
 
 };
 

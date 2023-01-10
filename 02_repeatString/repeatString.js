05bfcb9362aea49a57c01974e99643116e2f2a90
darizae <danielarizae@gmail.com>
2023-01-10 01:27:24 +0100

    Complete Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    if (num < 0) return "ERROR";
+
+    let toReturn = "";
+
+    for (var i = 0; i < num; i++) {
+        toReturn += str;
+    }
+
+    return toReturn;
 };
 
 // Do not edit below this line

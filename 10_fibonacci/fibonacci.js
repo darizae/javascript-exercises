8f2d442aec342e68ec9499078b8c8c0bebf36cd5
darizae <danielarizae@gmail.com>
2023-01-14 02:02:37 +0100

    Complete Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(limit) {
+    if (limit < 0) return "OOPS";
+    return generateFibonacci(limit)[limit-1];
 };
+
+function generateFibonacci(limit) {
+    let fibonacci = [1,1];
+
+    for (var i = 1; i < limit; i++) {
+        fibonacci.push(fibonacci[i] + fibonacci[i-1]);
+    }
+
+    return fibonacci;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;

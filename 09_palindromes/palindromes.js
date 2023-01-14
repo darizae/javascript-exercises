856c8b116b9a9f821a6a14cf264bba6a55d58de5
darizae <danielarizae@gmail.com>
2023-01-14 01:37:27 +0100

    Complete Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    cleanStr = removeNonLetters(str).toLowerCase();
+
+    let reverse = function(str) {
+        let toReturn = "";
+        for (let i = str.length - 1; i >= 0; i--) {
+            toReturn += str[i];
+        }
+        return toReturn;
+    }
+    return cleanStr === reverse(cleanStr);
 };
+
+function removeNonLetters(str) {
+    return str.replace(/[^a-zA-Z]/g, "");
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;

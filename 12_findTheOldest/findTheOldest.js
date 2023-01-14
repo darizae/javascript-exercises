2945f49198252d21c80c7dd5c033002858ac0c82
darizae <danielarizae@gmail.com>
2023-01-14 12:09:16 +0100

    Complete Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((prev, curr) => {
+        
+        if(!curr.hasOwnProperty("yearOfDeath")) {
+            curr.yearOfDeath = new Date().getFullYear();
+        }
+        if (!prev.hasOwnProperty("yearOfDeath")) {
+            prev.yearOfDeath = new Date().getFullYear();
+        }
 
+        return (curr.yearOfDeath - curr.yearOfBirth) > (prev.yearOfDeath - prev.yearOfBirth) ? curr : prev;
+    });
 };
 
 // Do not edit below this line
